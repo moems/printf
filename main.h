8fb1840910be5f0e74e391ee6b341d81cd2bb779
moems <mohammedgidado@hotmail.com>
2023-04-22 13:56:38 +0000

    created a main.h file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/* Prototypes */
+int _printf(const char *format, ...);
+
+#endif /* #ifndef MAIN_H */
