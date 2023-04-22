435af565917659a6a1794f5b699f4ceac031fffd
moems <mohammedgidado@hotmail.com>
2023-04-22 14:18:58 +0000

    added the prototype and discription to the printf file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - Writes a formatted output to standard output.
+ * @format: This is the chraracter string that is formatted
+ * and written to standard output.
+ *
+ * Return: The number of characters written to stdout.
+ */
+int _printf(const char *format, ...)
+{
+
+}
